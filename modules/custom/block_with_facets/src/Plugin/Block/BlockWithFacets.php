527aeb116ff5b09cc1ea6fc23e6849c04b6664d2
Alexey496 <alexey496@mail.ru>
2017-12-01 23:42:12 +0300

    facets by weight

--- modules/custom/block_with_facets/src/Plugin/Block/BlockWithFacets.php
+++ modules/custom/block_with_facets/src/Plugin/Block/BlockWithFacets.php
@@ -10,9 +10,7 @@
 namespace Drupal\block_with_facets\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\PluginBase;
-use Drupal\facets\Entity\Facet;
-use Drupal\facets\FacetManager\DefaultFacetManager;
+use Drupal\facets\FacetInterface;
 
 /**
  * Добавляем простой блок с текстом.
@@ -35,31 +33,38 @@ class BlockWithFacets extends BlockBase {
     /** @var \Drupal\facets\FacetInterface $facet */
     /** @var \Drupal\facets\FacetManager\DefaultFacetManager $facet_manager */
 
-    /*
-    * Укажем id источника фасетов, как правило это 'search_api:страница Views, созданная на основе поискового индекса'
-    */
+
+    // Укажем id источника фасетов, как правило это 'search_api:страница Views, созданная на основе поискового индекса'
     $facet_source_id = 'search_api:views_page__product_facet_search__page_1';
 
+    // Получим массив всех фасет из менеджера
     $facet_manager = \Drupal::service('facets.manager');
     $facets = $facet_manager->getFacetsByFacetSourceId($facet_source_id);
     $facet_manager->updateResults($facet_source_id);
+
+    // Отсортируем фасеты по весу
+    uasort($facets, function (FacetInterface $a, FacetInterface $b) {
+      if ($a->getWeight() == $b->getWeight()) {
+        return 0;
+      }
+      return ($a->getWeight() < $b->getWeight()) ? -1 : 1;
+    });
+
+    //соберем массив фасетов готовый для рендера
     $processed_facets = [];
     foreach ($facets as $key => $facet) {
       $processed_facets[$key] = $facet_manager->build($facet);
-      /*
-       * Добавим к фасете заголовок, соответствующий наименованию фасета
-       */
-      $processed_facets[$key][0]['#title'] = $facet->getName();
+      $processed_facets[$key][0]['#title'] = $facet->getName();// Добавим к фасете заголовок, соответствующий наименованию фасета
     }
-    /*
-     * Укажем конфигурацию блока
-     */
+
+    // Укажем конфигурацию блока
     $block = [
       '#type' => 'container',
       'element-content' => $processed_facets
     ];
     return $block;
-  } 
+  }
 }
 
- 
+
+
