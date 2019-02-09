a268d126616c1934cfaff87d546dafbb78be3364
alexey496 <alexey496@mail.ru>
2019-02-09 17:04:26 +0300

    price update sh

--- price_update.sh
+++ price_update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget ftp://diler2:FGfhn@85.249.224.228/PRD.xls -O ~/apart/public_html/sites/default/files/migration/PRD.xls
 cd ~/apart/public_html
-/opt/php71/bin/php ~/apart/public_html/vendor/drush/drush/drush migrate:import commerce_variations --update
+/usr/local/bin/drush_launcher.phar migrate:import commerce_variations --update
